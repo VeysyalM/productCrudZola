e8794466a4505d9dfc0e606b29b8e25fd7a6cdcf
VeysyalM <86783850+VeysyalM@users.noreply.github.com>
2021-09-10 15:16:23 -0400

    Update CartItemRepositoryTest.java

--- src/test/java/com/colin/CartItemRepositoryTest.java
+++ src/test/java/com/colin/CartItemRepositoryTest.java
@@ -1,136 +1,85 @@
 package com.colin;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 
 import com.colin.models.CartItem;
 import com.colin.models.Category;
 import com.colin.models.Product;
-import com.colin.models.ProductCategory;
 import com.colin.models.Role;
 import com.colin.models.User;
 import com.colin.repo.CartItemRepository;
-import com.colin.repo.ProductRepository;
-import com.colin.service.CartItemService;
-import com.colin.service.ProductService;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CartItemServiceTest {
+public class CartItemRepositoryTest {
 
-	@MockBean
+	@Autowired
 	CartItemRepository cartItemRepository;
 	
-	@Autowired
-	CartItemService cartItemService;
-	
 	@Test
-	public void testGetUserCart() {
-		ArrayList<CartItem> cartItems = new ArrayList<>();
-		Category c = new Category(1, "Dairy", new ArrayList<>());
-		Product p = new Product(1, "Milk", 3, 2.99, c, new ArrayList<>());
+	public void testGetCartItems() {
+		List<CartItem> cartItem = (List<CartItem>) cartItemRepository.findAll();
+		
+		assertTrue(cartItem.get(0).getQuantity() == 2);
+	}
+	
+	/*@Test
+	public void testSaveCartItem() {
+		Category c = new Category(1, "fruit", new ArrayList<>());
+		Product p = new Product(1, "apple", 5, 5, c, new ArrayList<>());
 		Set<Role> roles = new HashSet<>();
 		Role r = new Role();
 		r.setName("ROLE_USER");
 		roles.add(r);
-		User u = new User(1, "Percy", "pepperbox", roles, true, new ArrayList<>());
+		User u = new User(4, "Percy", "pepperbox", roles, true, new ArrayList<>());
+		BCryptPasswordEncoder b = new BCryptPasswordEncoder();
+		String encryptP = b.encode(u.getPassword());
+		u.setPassword(encryptP);
 		CartItem cI = new CartItem(1, 2, LocalDateTime.now(), u, p);
-		cartItems.add(cI);
 		
-		when(cartItemRepository.findAll()).thenReturn(cartItems);
-		
-		List<CartItem> userCart = cartItemService.getUserCart(1l);
-		
-		
-		assertEquals(userCart.size(), 1);
-		assertTrue(userCart.get(0).getProduct().getName().equals("Milk"));
-		assertTrue(userCart.get(0).getProduct().getQuantity() == 3);
-		assertTrue(userCart.get(0).getProduct().getPrice() == 2.99);
-		assertTrue(userCart.get(0).getProduct().getCategory().getName().equals("Dairy"));
-		assertTrue(userCart.get(0).getUser().getUsername().equals("Percy"));
-		assertTrue(userCart.get(0).getQuantity() == 2);
-		
-	}
+		CartItem cI2 = cartItemRepository.save(cI);
+		assertNotNull(cI2);
+		assertTrue(cI.getQuantity() == cI2.getQuantity());
+		assertTrue(cI.getProduct().getName().equals(cI2.getProduct().getName()));
+	}*/
 	
 	@Test
-	public void testCreateCartItem() {
-		Category c = new Category(1, "Dairy", new ArrayList<>());
-		Product p = new Product(1, "Milk", 3, 2.99, c, new ArrayList<>());
+	public void testDelteCartItem() {
+		Category c = new Category(1, "fruit", new ArrayList<>());
+		Product p = new Product(1, "apple", 5, 5, c, new ArrayList<>());
 		Set<Role> roles = new HashSet<>();
 		Role r = new Role();
 		r.setName("ROLE_USER");
 		roles.add(r);
-		User u = new User(1, "Percy", "pepperbox", roles, true, new ArrayList<>());
-		CartItem cI = new CartItem(1, 2, LocalDateTime.now(), u, p);
+		User u = new User(4, "Percy", "pepperbox", roles, true, new ArrayList<>());
+		BCryptPasswordEncoder b = new BCryptPasswordEncoder();
+		String encryptP = b.encode(u.getPassword());
+		u.setPassword(encryptP);
+		CartItem cI = new CartItem(20, 1, LocalDateTime.now(), u, p);
+		CartItem cI2 = cartItemRepository.save(cI);
 		
-		when(cartItemRepository.save(cI)).thenReturn(cI);
+		cartItemRepository.deleteById(cI2.getId());
 		
-		cartItemService.createCartItem(cI);
-		assertNotNull(cI);
+		assertNull(cartItemRepository.findById(20l).orElse(null));
+		
 	}
+
 	
-	@Test
-	public void testDeleteCartItemByUserId() {
-		Category c = new Category(1, "Dairy", new ArrayList<>());
-		Product p = new Product(1, "Milk", 3, 2.99, c, new ArrayList<>());
-		Set<Role> roles = new HashSet<>();
-		Role r = new Role();
-		r.setName("ROLE_USER");
-		roles.add(r);
-		User u = new User(1, "Percy", "pepperbox", roles, true, new ArrayList<>());
-		CartItem cI = new CartItem(1, 2, LocalDateTime.now(), u, p);
-		
-		when(cartItemRepository.save(cI)).thenReturn(cI);
-		
-		cartItemService.createCartItem(cI);
-		
-
-		
-		cartItemService.deleteByUserId(1l);
-		
-		assertNull(cartItemRepository.findById(1l).orElse(null));
-		
-
-	}
-	
-	@Test
-	public void testDeleteCartItemByUserAndProductId() {
-		Category c = new Category(1, "Dairy", new ArrayList<>());
-		Product p = new Product(1, "Milk", 3, 2.99, c, new ArrayList<>());
-		Set<Role> roles = new HashSet<>();
-		Role r = new Role();
-		r.setName("ROLE_USER");
-		roles.add(r);
-		User u = new User(1, "Percy", "pepperbox", roles, true, new ArrayList<>());
-		CartItem cI = new CartItem(1, 2, LocalDateTime.now(), u, p);
-		
-		when(cartItemRepository.save(cI)).thenReturn(cI);
-		
-		cartItemService.createCartItem(cI);
-		
-
-		
-		cartItemService.deleteByUserAndProductId(1l, 1l);
-		
-		assertNull(cartItemRepository.findById(1l).orElse(null));
-		
-
-	}
 }
